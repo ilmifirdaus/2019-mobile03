4538e79cf876f1acbe324b1be7b79875d62cab62
ilmifirdaus <firdausalya29@gmail.com>
2020-02-13 09:17:53 +0700

    Latihan Konversi Layout 1

---
+++ Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/WelcomeSlide.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.aplikasiku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeSlide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_slide);
+    }
+}
